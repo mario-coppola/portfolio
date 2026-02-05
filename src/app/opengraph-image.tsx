74e6eb932d560f5a3acae1ff886a531e442c6a78
mario <mariocoppo91@gmail.com>
2026-02-05 12:26:22 +0100

    chore: run OG image routes on nodejs

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from "next/og";
 import { site } from "@/content/site";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 
 export const size = {
   width: 1200,
