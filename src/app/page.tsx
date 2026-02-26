58872c45d850f8f3f14a0265b137e3944108306a
mario <mariocoppo91@gmail.com>
2026-02-26 17:10:25 +0100

    bug: fix og images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,6 @@ export async function generateMetadata({
     twitter: {
       card: "summary_large_image",
       title: site.title,
-      description: site.description,
       images: [globalOgImageUrl],
     },
   };
